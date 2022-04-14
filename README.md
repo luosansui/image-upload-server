f28e97eab85b46b373fa120243ea1f231bc5e431
luosansui <3099688664@qq.com>
2022-04-14 17:34:06 +0800

    bug fixed

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
+## 自建图床服务
 > + `npm i`
 > + 按照`/config/config.json`的信息创建对应数据库
 > + 打开`/utils/mountModel`里的`同步模型`注释，运行一次进行数据表同步
 > + 将`/utils/mountModel`里的`同步模型`再次注释掉
 > + `node app`起飞
+
+> 注: 如果遇到跨域问题请加载`koa-cors`包，附[链接](https://www.npmjs.com/package/koa-cors)
 
 ![](./public/image/20220413/upload_7df17b53f31070dc083e835ff70a015a.jpg)
--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+
+const path = require('path')
 const Koa = require('koa')
+const static = require('koa-static')
 //导入路由
 const img = require('./routes/img')
 //挂载模型
 const model = require('./utils/mountModel')
 //实例化对象
 const app = new Koa()
+//开放public
+app.use(static(path.join(__dirname, './public')))
 //图床服务
 app.use(img.routes())
 app.listen(4869)
